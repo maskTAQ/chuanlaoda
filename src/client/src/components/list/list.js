82e4ce0f1e24f1fc3b5ea435a3db808bd989007f
taiaq <50889574@qq.com>
2017-09-26 22:20:07 +0800

    封装list组件

---
+++ src/client/src/components/list/list.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import { findDOMNode } from 'react-dom';
+import BScroll from 'better-scroll';
+
+import { FontIcon } from 'material-ui';
+import moment from 'moment';
+
+
+import styles from './list.scss';
+
+export default class List extends Component {
+    componentDidMount() {
+        const dom = findDOMNode(this.refs.listContainer);
+        this.scroll = new BScroll(dom);
+
+    }
+    render() {
+        const { data } = this.props;
+        return (
+            <div ref="listContainer" style={{height:'100%'}}>
+                <ul className={styles.list}>
+                    {data.map((item, i) => {
+                        let {
+                origin,
+                            destination,
+                            cargoType,
+                            shipmentTime,
+                            createTime,
+                            cargoTonnage,
+                            type,
+                            _id } = item;
+                        shipmentTime = moment(createTime).format('M月D号');
+                        const typeMap = ['找货', '找船'];
+                        return (
+                            <li className={styles['list-item']} key={_id}>
+                                <div className={styles.title}>
+                                    <i className={styles.origin}>| {origin}</i>
+                                    <FontIcon className="material-icons" style={{ color: '#56e8f7', lineHeight: '20px', fontSize: '18px' }}>chevron_right</FontIcon>
+                                    <i className={styles.destination}>{destination}</i>
+                                    <i className={[styles.tag, styles['cargot-type']].join(' ')}>{cargoType}</i>
+                                    <i className={[styles.tag, styles.type].join(' ')}>{typeMap[type]}</i>
+                                    <i className={[styles.tag, styles['cargo-tonnage']].join(' ')}>{cargoTonnage}吨</i>
+                                </div>
+                                <div className={styles.detail}>
+                                    <span>
+                                        <i className={styles.label}>装货日期: </i>
+                                        <i className={styles.valu}>{shipmentTime}</i>
+                                    </span>
+                                </div>
+                            </li>
+                        )
+                    })}
+                </ul>
+            </div>
+        )
+    }
+}
--- src/client/src/page/home/home.js
+++ src/client/src/page/home/home.js
@@ -5,20 +5,13 @@ import axios from 'axios';
 import { Dialog, FlatButton, FontIcon } from 'material-ui';
 import moment from 'moment';
 
+import List from 'components/list/list.js';
 import { Api } from 'src/config.js';
 import styles from './home.scss';
 
 export default class Home extends Component {
     componentDidMount() {
         this.getData();
-    }
-    componentDidUpdate() {
-        const { status } = this.state;
-        if (status === 'success') {
-            const dom = findDOMNode(this.refs.containerList);
-            let scroll = new BScroll(dom);
-        }
-
     }
     state = {
         open: false,
@@ -51,48 +44,12 @@ export default class Home extends Component {
     }
     render() {
         const { status, data } = this.state;
-        const actions = [(<FlatButton label="Cancel" primary={true} onClick={this.handleClose} />), (<FlatButton label="Discard" primary={true} onClick={this.handleClose} />)];
-
-        const typeMap = ['找货', '找船'];
-
+        const actions = [(<FlatButton label="取消" primary={true} onClick={this.handleClose} />), (<FlatButton label="重试" primary={true} onClick={this.handleClose} />)];
         switch (status) {
             case 'success':
                 return (
                     <div className={styles.container} ref="containerList">
-                        <ul className={styles.list} ref="list">
-                            {data.map((item, i) => {
-                                let {
-                                    origin,
-                                    destination,
-                                    cargoType,
-                                    shipmentTime,
-                                    createTime,
-                                    cargoTonnage,
-                                    type,
-                                    _id
-                                } = item;
-                                shipmentTime = moment(createTime).format('M月D号');
-                                return (
-                                    <li className={styles['list-item']} key={_id}>
-                                        <div className={styles.title}>
-                                            <i className={styles.origin}>{origin}</i>
-                                            <FontIcon className="material-icons" style={{ color: '#56e8f7', lineHeight: '20px', fontSize: '18px' }}>chevron_right</FontIcon>
-                                            <i className={styles.destination}>{destination}</i>
-                                            <i className={[styles.tag, styles['cargot-type']].join(' ')}>{cargoType}</i>
-                                            <i className={[styles.tag, styles.type].join(' ')}>{typeMap[type]}</i>
-                                            <i className={[styles.tag, styles['cargo-tonnage']].join(' ')}>{cargoTonnage}吨</i>
-                                        </div>
-                                        <div className={styles.detail}>
-                                            <span>
-                                                <i className={styles.label}>装货日期: </i>
-                                                <i className={styles.valu}>{shipmentTime}</i>
-                                            </span>
-                                        </div>
-                                    </li>
-                                )
-                            })}
-                        </ul>
-
+                        <List data={data} />
                     </div>
                 );
             case 'init':
@@ -101,13 +58,15 @@ export default class Home extends Component {
                 );
             default:
                 return (
-                    <Dialog
-                        actions={actions}
-                        modal={false}
-                        open={this.state.open}
-                        onRequestClose={this.handleClose}>
-                        数据加载失败点我重试
-                    </Dialog>
+                    <div className={styles.container} ref="containerList">
+                        <Dialog
+                            actions={actions}
+                            modal={false}
+                            open={this.state.open}
+                            onRequestClose={this.handleClose}>
+                            数据加载失败,是否重试?
+                         </Dialog>
+                    </div>
                 )
         }
 
